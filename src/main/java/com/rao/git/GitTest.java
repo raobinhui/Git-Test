9b34973a269e5c7deceb39c2c819b51088f468f3
rao <2981171740@qq.com>
2022-05-30 17:53:35 +0800

    第二次提交

--- src/main/java/com/rao/git/GitTest.java
+++ src/main/java/com/rao/git/GitTest.java
@@ -3,5 +3,6 @@ package com.rao.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello  Git");
+        System.out.println("Hello  GitHub");
     }
 }
