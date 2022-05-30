be6dd68b22336bb916668f93e7e4a720a68f4ad0
饶斌辉 <2981171740@qq.com>
2022-05-30 13:54:16 +0000

    update src/main/java/com/rao/git/GitTest.java.

--- src/main/java/com/rao/git/GitTest.java
+++ src/main/java/com/rao/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("Push Test！");
         System.out.println("Pull Test！");
         System.out.println("Gitee Test！");
+        System.out.println("Gitee Test2！");
     }
 }
