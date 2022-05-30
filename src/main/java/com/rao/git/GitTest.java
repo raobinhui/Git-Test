17abc48d247022719e047c74070f5460c8f5810b
rao <2981171740@qq.com>
2022-05-30 18:02:39 +0800

    Hot-fix第一次提交

--- src/main/java/com/rao/git/GitTest.java
+++ src/main/java/com/rao/git/GitTest.java
@@ -2,8 +2,9 @@ package com.rao.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello  Git");
+        System .out.println("Hello  Git");
         System.out.println("Hello  GitHub");
         System.out.println("Hello  GitLab");
+        System.out.println("Hello  GitLab牛逼！");
     }
 }
