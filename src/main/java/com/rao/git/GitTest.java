13a37862a0a736baf3443a8bb05ab4df984741ef
rao <2981171740@qq.com>
2022-05-30 19:52:39 +0800

    Push第一次提交

--- src/main/java/com/rao/git/GitTest.java
+++ src/main/java/com/rao/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello  GitLab牛逼！");
         System.out.println("master   Test！");
         System.out.println("Hot-Fix Test！");
+        System.out.println("Push Test！");
     }
 }
